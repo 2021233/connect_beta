99a1e334012877813b71bc58e22cc0b85f446daf
negi <eadxNM297@gmail.com>
2024-02-06 16:54:57 +0900

    sample mode change function

--- docs/samples/sample.js
+++ docs/samples/sample.js
@@ -2,48 +2,52 @@ const cameraWidth = 300;
 const cameraHeight = 400;
 
 const cameraInitSmartphoneSupport = () => {
-    const video = document.getElementById("camera");
+  const video = document.getElementById("camera");
 
-    //スマホからの閲覧か
-    const isMobile = navigator.userAgent.match(/iPhone|Android/);
+  //スマホからの閲覧か
+  const isMobile = navigator.userAgent.match(/iPhone|Android/);
+  let mode = mode === "user" ? "enviroment" : "user";
 
-    const cameraSetting = {
-        audio: false,
-        video: {
-            //スマホの場合は縦横を逆に設定する
-            width: isMobile ? cameraHeight : cameraWidth,
-            height: isMobile ? cameraWidth : cameraHeight,
-            facingMode: "user",
-        }
-    }
+  const cameraSetting = {
+    audio: false,
+    video: {
+      //スマホの場合は縦横を逆に設定する
+      width: isMobile ? cameraHeight : cameraWidth,
+      height: isMobile ? cameraWidth : cameraHeight,
+      facingMode: mode,
+    },
+  };
 
-    navigator.mediaDevices.getUserMedia(cameraSetting)
-        .then((mediaStream) => {
-            video.srcObject = mediaStream;
-            console.log(mediaStream)
-        })
-        .catch((err) => {
-            console.log(err.toString());
-        });
-}
+  navigator.mediaDevices
+    .getUserMedia(cameraSetting)
+    .then((mediaStream) => {
+      video.srcObject = mediaStream;
+      console.log(mediaStream);
+      console.log(cameraSetting);
+      console.log(video.srcObject);
+    })
+    .catch((err) => {
+      console.log(err.toString());
+    });
+};
 
 const shoot = () => {
-    //video要素
-    const video = document.getElementById("camera");
-    //canvas要素
-    const canvas = document.getElementById("canvas");
-    //canvas要素の大きさを変更
-    canvas.width = cameraWidth;
-    canvas.height = cameraHeight;
-    //描画用オブジェクトを取得
-    const ctx = canvas.getContext("2d");
+  //video要素
+  const video = document.getElementById("camera");
+  //canvas要素
+  const canvas = document.getElementById("canvas");
+  //canvas要素の大きさを変更
+  canvas.width = cameraWidth;
+  canvas.height = cameraHeight;
+  //描画用オブジェクトを取得
+  const ctx = canvas.getContext("2d");
 
-    //描画する
-    ctx.drawImage(
-        video,          // データソース 
-        0,              // 描画開始x座標  
-        0,              // 描画開始y座標
-        cameraWidth,    // 描画横サイズ
-        cameraHeight    // 描画縦サイズ
-    );
-}
+  //描画する
+  ctx.drawImage(
+    video, // データソース
+    0, // 描画開始x座標
+    0, // 描画開始y座標
+    cameraWidth, // 描画横サイズ
+    cameraHeight // 描画縦サイズ
+  );
+};
